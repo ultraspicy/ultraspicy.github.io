86d649ded137e58922fc790dce77e3ea490f8605
Jianfeng Guo <element.no.88@gmail.com>
2026-08-15 11:14:09 -0700

    doc: add selfie

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Edit these to make the site yours.
-export const SITE_TITLE = 'ed25519';
+export const SITE_TITLE = 'Jianfeng Guo';
 export const SITE_DESCRIPTION = 'Personal blog.';
 export const NAV = [
   { href: '/', label: 'Home' },
